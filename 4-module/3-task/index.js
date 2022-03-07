ad92ca88d20dac63ca684ffb743c6eebf92dc48e
Toma <akietsuko@gmail.com>
2022-03-07 21:31:27 +0300

    add highlight function

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,38 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i]
+    
+    const statusCell = row.cells[3]
+    const dataAvailable = statusCell.dataset.available
+    switch(dataAvailable) {
+      case undefined: {
+        row.hidden = true
+        break
+      }
+      case 'false': {
+        row.classList.add('unavailable')
+        break
+      }
+      case 'true': {
+        row.classList.add('available')
+        break
+      }
+    }
+    
+    const genderCell = row.cells[2].innerText
+    switch(genderCell) {
+      case ('m'): {
+        row.classList.add('male')
+        break
+      }
+       case ('f'): {
+        row.classList.add('female')
+      }
+    }
+    
+    const ageCell = row.cells[1].innerText
+    if (ageCell < 18) {
+      row.style.textDecoration = 'line-through'
+    }
+  }
 }
