edcb0d71788ff21148e808ed83f08dfc2dd95f2b
Toma <akietsuko@gmail.com>
2022-03-17 14:41:19 +0300

    remove refactor

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,28 +1,28 @@
 function initCarousel() {
-  var carouselArrowRight = document.querySelector('.carousel__arrow_right')
-  var carouselArrowLeft = document.querySelector('.carousel__arrow_left')
+  const carouselArrowRight = document.querySelector('.carousel__arrow_right')
+  const carouselArrowLeft = document.querySelector('.carousel__arrow_left')
 
-  var container = document.querySelector('.carousel__inner')
-  var offsetWidth = container.offsetWidth
-  var slides = Array.from(document.querySelectorAll('.carousel__slide'))
-  var currentSlideIndex = 0
+  const container = document.querySelector('.carousel__inner')
+  const offsetWidth = container.offsetWidth
+  const slides = Array.from(document.querySelectorAll('.carousel__slide'))
+  let currentSlideIndex = 0
 
-  const setArrowStyle = () => {
-    carouselArrowLeft.style.display = currentSlideIndex === 0 ? 'none' : ''
-    carouselArrowRight.style.display = currentSlideIndex === slides.length - 1 ? 'none' : ''
-  }
-
-  setArrowStyle()
+  setArrowStyle(carouselArrowLeft, carouselArrowRight, currentSlideIndex, slides.length)
 
   carouselArrowRight.addEventListener('click', (event) => {
     currentSlideIndex += 1
     container.style.transform = `translateX(-${offsetWidth * currentSlideIndex}px)`
-    setArrowStyle()
+    setArrowStyle(carouselArrowLeft, carouselArrowRight, currentSlideIndex, slides.length)
   })
 
   carouselArrowLeft.addEventListener('click', () => {
     currentSlideIndex -= 1
     container.style.transform = `translateX(-${offsetWidth * currentSlideIndex}px)`
-    setArrowStyle()
+    setArrowStyle(carouselArrowLeft, carouselArrowRight, currentSlideIndex, slides.length)
   })
+}
+
+const setArrowStyle = (arrowLeft, arrowRight, currentSlideIndex, slidesCount) => {
+  arrowLeft.style.display = currentSlideIndex === 0 ? 'none' : ''
+  arrowRight.style.display = currentSlideIndex === slidesCount - 1 ? 'none' : ''
 }
