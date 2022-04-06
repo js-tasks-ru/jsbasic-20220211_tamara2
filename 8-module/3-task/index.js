53f84b89f03d2938db4dc1c114177747ef32e888
Toma <akietsuko@gmail.com>
2022-04-06 14:52:56 +0300

    add cart 1 part

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,40 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return
+    }
+    let itemIndex = this.cartItems.findIndex(item => item.product.id === product.id)
+    console.log(itemIndex)
+    if (itemIndex !== -1) {
+      this.cartItems[itemIndex].count++
+    } else {
+      this.cartItems.push({product, count: 1})
+      itemIndex = this.cartItems.length - 1
+
+    }
+    this.onProductUpdate(this.cartItems[itemIndex]);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let itemIndex = this.cartItems.findIndex(item => item.product.id === productId)
+    this.cartItems[itemIndex].count += amount
+    if (this.cartItems[itemIndex].count === 0) {
+      this.cartItems = this.cartItems.filter(item => item.product.id !== productId)
+    }
+    this.onProductUpdate(this.cartItems[itemIndex])
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((sum, item) => sum += item.count, 0)
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((sum, item) => sum += item.count * item.product.price, 0)
   }
 
   onProductUpdate(cartItem) {
